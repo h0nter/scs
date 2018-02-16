98ae0d3c3b253a5f32da909c5736b29067b77f3c
h0nter <34107939+h0nter@users.noreply.github.com>
2018-02-16 00:58:47 +0000

    JS files and slight parallax on landing page
    Created functions.js and jquery files. Introduced slight parallax effect on landing page, using CSS.

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+$(window).scroll(function(){
+
+  var wScroll= $(this).scrollTop();
+
+  /*$('#header').css({
+    'transform' : 'scale(0.'+ (1 + scroll/8) +',0.'+ (1 + wScroll/8) +')'
+  })*/
+
+});
