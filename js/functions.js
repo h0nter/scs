77b09c3a6c691427150ec041f0851c6dc8c4144a
h0nter <34107939+h0nter@users.noreply.github.com>
2018-02-27 01:08:32 +0000

    Overlay booking form set-up
    Set-up basing styling and JavaScript to toggle the overlay booking form on and off. Changed email input type from 'text' to 'email'

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,11 @@ $(window).scroll(function(){
   })*/
 
 });
+
+function on() {
+    document.getElementById("overlay").style.display = "flex";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
