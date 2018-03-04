8064281b2585f6b4cbedae54b8474b45567ad157
h0nter <34107939+h0nter@users.noreply.github.com>
2018-03-04 21:01:51 +0000

    'Password reset' form validation
    Implemented client side (JS) validation on the 'Password reset' page.

---
+++ js/passreset_val.js
@@ -0,0 +1,13 @@
+function valEmailReset(){
+  var email = document.forms["pass-reset-f"]["email-address"].value;
+  const emailObj = document.getElementById("pass-reset-email");
+  const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
+
+  if (regex.test(email)){
+    return true;
+  }else{
+    emailObj.classList.remove('valid-field');
+    emailObj.classList.add('invalid-field');
+    return false;
+  }
+}
