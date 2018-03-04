3fbcff5fc9f1405f6bdaaa970de7513ea29f21a5
h0nter <34107939+h0nter@users.noreply.github.com>
2018-03-04 01:44:59 +0000

    Booking form validation
    Client-side (JS) booking form validation, incorrect fields styling reset and id's added to elements in "details_edit.html"

---
+++ js/booking_form_val.js
@@ -0,0 +1,157 @@
+function valYearOfProd(){
+  var yop = document.forms["b-form"]["year"].value;
+  const yearProd = document.getElementById("year-prod");
+  const regex = /^((19||20)+([0-9]{2}))$/;
+
+  if (regex.test(yop)){
+    return true;
+  }else{
+    yearProd.classList.remove('valid-field');
+    yearProd.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valForename(){
+  var forename = document.forms["b-form"]["forename"].value;
+  const forenameObj = document.getElementById("booking-forename");
+  const regex = /^[a-zA-Z]{2,30}$/;
+
+  if (regex.test(forename)){
+    return true;
+  }else{
+    forenameObj.classList.remove('valid-field');
+    forenameObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valSurname(){
+  var surname = document.forms["b-form"]["surname"].value;
+  const surnameObj = document.getElementById("booking-surname");
+  const regex = /^[a-zA-Z]{2,30}$/;
+
+  if (regex.test(surname)){
+    return true;
+  }else{
+    surnameObj.classList.remove('valid-field');
+    surnameObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valEmail(){
+  var email = document.forms["b-form"]["email-address"].value;
+  const emailObj = document.getElementById("booking-email");
+  const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b/;
+
+  if (regex.test(email)){
+    return true;
+  }else{
+    emailObj.classList.remove('valid-field');
+    emailObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valPhoneNum(){
+  var phoneNum = document.forms["b-form"]["phone-num"].value;
+  const phoneNumObj = document.getElementById("booking-phone-num");
+  const regex = /^[0-9]{10,14}$/;
+
+  if (regex.test(phoneNum)){
+    return true;
+  }else{
+    phoneNumObj.classList.remove('valid-field');
+    phoneNumObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valAddress(){
+  var address = document.forms["b-form"]["address-line1"].value;
+  const addressObj = document.getElementById("booking-addl1");
+  const regex = /^[a-zA-Z0-9]{2,50}$/;
+
+  if (regex.test(address)){
+    return true;
+  }else{
+    addressObj.classList.remove('valid-field');
+    addressObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valAddress2(){
+  var address2 = document.forms["b-form"]["address-line2"].value;
+  const address2Obj = document.getElementById("booking-addl2");
+  const regex = /^[a-zA-Z0-9]{2,50}$/;
+
+  if (regex.test(address2) || address2 == ""){
+    return true;
+  }else{
+    address2Obj.classList.remove('valid-field');
+    address2Obj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valCity(){
+  var city = document.forms["b-form"]["city"].value;
+  const cityObj = document.getElementById("booking-city");
+  const regex = /^[a-zA-Z]{2,20}$/;
+
+  if (regex.test(city)){
+    return true;
+  }else{
+    cityObj.classList.remove('valid-field');
+    cityObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valPostcode(){
+  var postcode = document.forms["b-form"]["postcode"].value;
+  const postcodeObj = document.getElementById("booking-postcode");
+  const regex = /^(([a-zA-Z]{2}\d[a-zA-Z]\s\d[a-zA-Z]{2})|([a-zA-Z]\d[a-zA-Z]\s\d[a-zA-Z]{2})|([a-zA-Z]\d\s\d[a-zA-Z]{2})|([a-zA-Z]\d{2}\s\d[a-zA-Z]{2})|([a-zA-Z]{2}\d\s\d[a-zA-Z]{2})|([a-zA-Z]{2}\d{2}\s\d[a-zA-Z]{2}))$/;
+
+  if (regex.test(postcode)){
+    return true;
+  }else{
+    postcodeObj.classList.remove('valid-field');
+    postcodeObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valDescription(){
+  var description = document.forms["b-form"]["service-description"].value;
+  const descriptionObj = document.getElementById("service-description");
+  const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]{3,1000}$/;
+
+  if (regex.test(description)){
+    return true;
+  }else{
+    descriptionObj.classList.remove('valid-field');
+    descriptionObj.classList.add('invalid-field');
+    return false;
+  }
+}
+
+function valCompType(){
+  var comptype = document.forms["b-form"]["computer-type"].value;
+  const comptypeObj = document.getElementById("computer-type");
+
+  if (comptype == "Macbook" || comptype == "iMac"){
+    document.getElementById("comp-make").innerHTML = "<option value=\"Apple\">Apple</option>";
+  }else if(comptype != ""){
+    document.getElementById("comp-make").innerHTML = "<option></option>\
+    <option value=\"HP\">HP</option>\
+    <option value=\"Lenovo\">Lenovo</option>\
+    <option value=\"Dell\">Dell</option>\
+    <option value=\"Asus\">Asus</option>\
+    <option value=\"Acer\">Acer</option>\
+    <option value=\"Alienware\">Alienware</option>\
+    <option value=\"Other\">Other</option>";
+  }
+}
--- js/functions.js
+++ js/functions.js
@@ -15,6 +15,16 @@ function on() {
 }
 
 function off() {
+    //set up array with fields selectors
+    var resetObjects = [document.getElementById("year-prod"),document.getElementById("booking-forename"),document.getElementById("booking-surname"),document.getElementById("booking-email"),document.getElementById("booking-phone-num"),document.getElementById("booking-addl1"),document.getElementById("booking-addl2"),document.getElementById("booking-city"),document.getElementById("booking-postcode"),document.getElementById("service-description")]
+
     document.getElementById("overlay").style.display = "none"; //close booking form
+
+    //reset the styling of the form fields
+    for (i=0;i<10;i++){
+      resetObjects[i].classList.remove("invalid-field");
+      resetObjects[i].classList.add("valid-field");
+    }
+
     document.getElementById("b-form").reset(); //reset booking form
 }
