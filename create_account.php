480d26d3c9c2c96db9e65bed05c78fca6000a848
h0nter <34107939+h0nter@users.noreply.github.com>
2018-03-11 21:21:18 +0000

    New account register and sign-in
    Implemented a fully working new account register functionality and improved sign-in page.

--- create_account.php
+++ create_account.php
@@ -1,3 +1,162 @@
+<?php
+
+  session_start();//Start user session for global variables
+
+  //Check if the form has been submited
+  if (isset($_POST['forename'])){
+    //Successful validation flag
+    $successVal = true;
+
+    //Check if forename is valid
+    $forename = $_POST['forename'];
+    $regex = "/^[a-zA-Z]{2,30}$/";
+
+    if (!preg_match($regex, $forename)){
+      $successVal = false;
+      $_SESSION['e_forename'] = "Forename has to be between 2 and 30 characters and cosist of letters only!";
+    }
+
+    //Check if surname is valid
+    $surname = $_POST['surname'];
+    $regex = "/^([a-zA-Z]+[\s]?[-\s]?[\s]?[a-zA-Z]{1,29})$/";
+
+    if (!preg_match($regex, $surname)){
+      $successVal = false;
+      $_SESSION['e_surname'] = "Surname has to be between 2 and 30 characters! Use dash to separate double-barrelled surnames.";
+    }
+
+    //Check if email address is valid
+    $email = $_POST['email-address'];//Store user email input in variable
+    $emailS = filter_var($email, FILTER_SANITIZE_EMAIL);//Sanitize email
+
+    //Validate sanitized email and compare it to the original user input
+    if ((filter_var($emailS, FILTER_VALIDATE_EMAIL) == false) || ($emailS != $email)){
+      $successVal = false;
+      $_SESSION['e_email'] = "Input correct email address!";//Email error message
+    }
+
+    //Check if the passwords are valid
+    $pass1 = $_POST['u-password'];
+    $pass2 = $_POST['pass-confirm'];
+    $regex = "/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d!#$%&'*+=?^_-].{8,}$/";
+
+    if (!preg_match($regex, $pass1)){
+      $successVal = false;
+      $_SESSION['e_password'] = "Password must have more than 8 characters, at least 1 capital letter and at least 1 number!";
+    }
+
+    if ($pass1 != $pass2){
+      $successVal = false;
+      $_SESSION['e_password'] = "Entered passwords aren't identical";
+    }
+
+    $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
+
+    //Check if the phone number is valid
+    $phone_num = $_POST['phone-num'];
+    $regex = "/^[0-9]{11,14}$/";
+
+    if ((!preg_match($regex, $phone_num)) && $phone_num != ""){
+      $successVal = false;
+      $_SESSION['e_phonenum'] = "Phone number must contain numbers only and be 11-14 digits long!";
+    }
+
+    //Check if the address (line 1) is valid
+    $address = $_POST['address-line1'];
+    $regex = "/^[a-zA-Z0-9\s]{2,50}$/";
+
+    if ((!preg_match($regex, $address)) && $address != ""){
+      $successVal = false;
+      $_SESSION['e_address1'] = "Address can contain letters, numbers and spaces only and cannot be longer than 50 characters!";
+    }
+
+    //Check if the address (line 2) is valid
+    $address2 = $_POST['address-line2'];
+    $regex = "/^[a-zA-Z0-9\s]{2,50}$/";
+
+    if ((!preg_match($regex, $address2)) && $address2 != ""){
+      $successVal = false;
+      $_SESSION['e_address2'] = "Address can contain letters, numbers and spaces only and cannot be longer than 50 characters!";
+    }
+
+    //Check if the city is valid
+    $city = $_POST['city'];
+    $regex = "/^[a-zA-Z]{2,20}$/";
+
+    if ((!preg_match($regex, $city)) && $city != ""){
+      $successVal = false;
+      $_SESSION['e_city'] = "City can containe letters only and cannot be longer than 20 characters!";
+    }
+
+    //Check if the postcode is valid
+    $postcode = $_POST['postcode'];
+    $regex = "/^(([a-zA-Z]{2}\d[a-zA-Z]\s\d[a-zA-Z]{2})|([a-zA-Z]\d[a-zA-Z]\s\d[a-zA-Z]{2})|([a-zA-Z]\d\s\d[a-zA-Z]{2})|([a-zA-Z]\d{2}\s\d[a-zA-Z]{2})|([a-zA-Z]{2}\d\s\d[a-zA-Z]{2})|([a-zA-Z]{2}\d{2}\s\d[a-zA-Z]{2}))$/";
+
+    if ((!preg_match($regex, $postcode)) && $postcode != ""){
+      $successVal = false;
+      $_SESSION['e_postcode'] = "Enter a correct UK postcode";
+    }
+
+    //Check if checkbox was clicked
+    if (!isset($_POST['terms-cond'])){
+      $successVal == false;
+      $_SESSION['e_checkbox'] = "Agree to terms and conditions!";
+    }
+
+    //Bot or not - reCAPTCHA
+    $secret = "6Ldi7UsUAAAAAMUwFp4_jf02tbZWUSDpVANkZy_t";
+
+    $check_CAPTCHA = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+
+    $answer_CAPTCHA = json_decode($check_CAPTCHA);
+
+    if ($answer_CAPTCHA->success == false){
+      $successVal = false;
+      $_SESSION['e_bot'] = "Confirm that you're a human!";
+    }
+
+    require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);//Instead of warnings, throw exceptions
+
+    try{
+      $connection = new mysqli($host, $db_user, $db_password, $db_name);
+      if ($connection->connect_errno!=0){
+        throw new Exception(mysqli_connect_errno());
+      }else{
+        //Check if the email exists in the db
+        $result = $connection->query("SELECT user_id FROM users WHERE email='$email'");
+
+        if (!$result){
+          throw new Exception($connection->error);
+        }
+
+        $emailsNum = $result->num_rows;
+        if($emailsNum>0){
+          $successVal = false;
+          $_SESSION['e_email'] = "Email you're trying to use is already assigned to another account!";
+        }
+
+        if($successVal == true){
+          //Validation IS successful, add user to the db
+          if ($connection->query("INSERT INTO users VALUES (NULL, '$forename','$surname','$email','$pass_hash','$phone_num','$address','$address2','$city','$postcode')")){
+            $_SESSION['successReg']=true;
+            header('Location: welcome.php');
+          }else{
+            throw new Exception($connection->error);
+          }
+        }
+
+        $connection->close();
+      }
+    }
+    catch(Exception $e){
+      echo '<span style="color: red;">Server error! Sorry for the inconvinience, please try again at a different time.</span>';
+      //echo '<br />Developer info: '.$e;
+    }
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +178,11 @@
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/functions.js"></script>
   <script src="js/create_account_val.js"></script>
+
+  <!-- reCAPTCHA
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+
   <!-- Page icon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="icon" type="image/png" href="images/favicon.png">
@@ -172,15 +336,43 @@
     </div>
 
     <div id="account-create-form">
-      <form id="account-create-f" onsubmit="return !!(valForenameAcc() & valSurnameAcc() & valEmailAcc() & valPasswords() & valPhoneNumAcc() & valAddressAcc() & valAddress2Acc() & valCityAcc() & valPostcodeAcc());">
+      <form id="account-create-f" method="post" onsubmit="return !!(valForenameAcc() & valSurnameAcc() & valEmailAcc() & valPasswords() & valPhoneNumAcc() & valAddressAcc() & valAddress2Acc() & valCityAcc() & valPostcodeAcc());">
         <div id="required-data">
           <label for="forename" class="d-form-label">Forename<span style="color: red;">*</span>:</label> <input type="text" name="forename" id="new-account-forename" required><br>
 
+          <?php
+            if (isset($_SESSION['e_forename'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_forename'].'</div>';
+              unset($_SESSION['e_forename']);
+            }
+          ?>
+
           <label for="surname" class="d-form-label">Surname<span style="color: red;">*</span>:</label><input type="text" name="surname" id="new-account-surname" required><br>
+
+          <?php
+            if (isset($_SESSION['e_surname'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_surname'].'</div>';
+              unset($_SESSION['e_surname']);
+            }
+          ?>
 
           <label for="email-address" class="d-form-label">E-mail address<span style="color: red;">*</span>:</label><input type="email" name="email-address" id="new-account-email" required><br>
 
+          <?php
+            if (isset($_SESSION['e_email'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_email'].'</div>';
+              unset($_SESSION['e_email']);
+            }
+          ?>
+
           <label for="u-password" class="d-form-label">Password<span style="color: red;">*</span>:</label><input type="password" name="u-password" id="new-account-password" required><br>
+
+          <?php
+            if (isset($_SESSION['e_password'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_password'].'</div>';
+              unset($_SESSION['e_password']);
+            }
+          ?>
 
           <label for="pass-confirm" class="d-form-label">Confirm password<span style="color: red;">*</span>:</label><input type="password" name="pass-confirm" id="new-account-passconf" required><br>
         </div>
@@ -190,13 +382,67 @@
         <div id="additional-data">
           <label for="phone-num" class="d-form-label">Phone Number:</label><input type="text" name="phone-num" id="new-account-phonenum"><br>
 
+          <?php
+            if (isset($_SESSION['e_phonenum'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_phonenum'].'</div>';
+              unset($_SESSION['e_phonenum']);
+            }
+          ?>
+
           <label for="address-line1" class="d-form-label">Address:</label><input type="text" name="address-line1" id="new-account-addl1"><br>
+
+          <?php
+            if (isset($_SESSION['e_address1'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_address1'].'</div>';
+              unset($_SESSION['e_address1']);
+            }
+          ?>
 
           <label for="address-line2" class="d-form-label">Address (line 2):</label><input type="text" name="address-line2" id="new-account-addl2"><br>
 
+          <?php
+            if (isset($_SESSION['e_address2'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_address2'].'</div>';
+              unset($_SESSION['e_address2']);
+            }
+          ?>
+
           <label for="city" class="d-form-label">City:</label><input type="text" name="city" id="new-account-city"><br>
 
+          <?php
+            if (isset($_SESSION['e_city'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_city'].'</div>';
+              unset($_SESSION['e_city']);
+            }
+          ?>
+
           <label for="postcode" class="d-form-label">Postcode:</label><input type="text" name="postcode" id="new-account-postcode" style="max-width: 15rem;"><br>
+
+          <?php
+            if (isset($_SESSION['e_postcode'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_postcode'].'</div>';
+              unset($_SESSION['e_postcode']);
+            }
+          ?>
+
+          <label>
+          <input type="checkbox" name="terms-cond" required/>I agree to terms and conditions</label>
+
+          <?php
+            if (isset($_SESSION['e_checkbox'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_checkbox'].'</div>';
+              unset($_SESSION['e_checkbox']);
+            }
+          ?>
+
+          <div class="g-recaptcha" data-sitekey="6Ldi7UsUAAAAAIMr-M7_X5dvI6b4w0CIcVYP0Tin"></div>
+
+          <?php
+            if (isset($_SESSION['e_bot'])){
+              echo '<div style="color:red; font-size: 1.7rem;">'.$_SESSION['e_bot'].'</div>';
+              unset($_SESSION['e_bot']);
+            }
+          ?>
 
           <input type="submit" value="Create your account">
         </div>
--- login.php
+++ login.php
@@ -9,50 +9,62 @@
   }
 
   require_once "connect.php";//Attach 'connect.php' file
+  mysqli_report(MYSQLI_REPORT_STRICT);//Instead of warnings, throw exceptions
 
-  //Establish a new database connection
-  $connection = @new mysqli($host, $db_user, $db_password, $db_name);
+  try{
+    $connection = new mysqli($host, $db_user, $db_password, $db_name);//Establish a new database connection
+    //Check if the connection with database was successful
+    if($connection->connect_errno!=0){
+      throw new Exception(mysqli_connect_errno());//In case of connection error, throw an exception
+    }else{
+      //Set-up variables for email and password input by user in sign-in form
+      $email = $_POST['email-address'];
+      $password = $_POST['user-password'];
 
-  //Check if the connection with database was successful
-  if($connection->connect_errno!=0){
-    echo "Error: ".$connection->connect_errno;//In case of an error, show the error number.
-  }else{
-    //Set-up variables for email and password input by user in sign-in form
-    $email = $_POST['email-address'];
-    $password = $_POST['user-password'];
+      //Sanitize email and password (replace any '' and "" with HTML entities)
+      $email = htmlentities($email, ENT_QUOTES, "UTF-8");
 
-    //Sanitize email and password (replace any '' and "" with HTML entities)
-    $email = htmlentities($email, ENT_QUOTES, "UTF-8");
-    $password = htmlentities($password, ENT_QUOTES, "UTF-8");
+      $db_result = $connection->query(sprintf("SELECT * FROM users WHERE email='%s'", mysqli_real_escape_string($connection,$email)));//Variable to store result of the SQL query which selects the user from the db
 
-    if ($db_result = @$connection->query(sprintf("SELECT * FROM users WHERE email='%s' AND password='%s'", mysqli_real_escape_string($connection,$email), mysqli_real_escape_string($connection,$password)))){//Variable to store result of the SQL query which selects the user from the db
+      if (!$db_result){
+        throw new Exception($connection->error);
+      }
 
       $users_num = $db_result->num_rows;//Number of users found in the db
       if ($users_num > 0){//Check if any users were found in the db
-        $_SESSION['loggedIn'] = true;//User is signed in
-
         $record = $db_result->fetch_assoc();//Store user record data in an associative table
 
-        //Assign values from the record table to global variables
-        $_SESSION['user_id'] = $record['user_id'];
-        $_SESSION['first_name'] = $record['first_name'];
-        $_SESSION['last_name'] = $record['last_name'];
-        $_SESSION['email'] = $record['email'];
-        $_SESSION['phone_num'] = $record['phone_num'];
-        $_SESSION['address'] = $record['address'];
-        $_SESSION['address2'] = $record['address2'];
-        $_SESSION['city'] = $record['city'];
-        $_SESSION['postcode'] = $record['postcode'];
+        if(password_verify($password, $record['password'])){
+          $_SESSION['loggedIn'] = true;//User is signed in
 
-        unset($_SESSION['loginError']);//Remove log-in error variable
-        $db_result->free_result();//Free up the space in memory 'locked' by the SQL query result variable
-        header('Location: myaccount.php');//Login is successful, send user to their account page
+          //Assign values from the record table to global variables
+          $_SESSION['user_id'] = $record['user_id'];
+          $_SESSION['first_name'] = $record['first_name'];
+          $_SESSION['last_name'] = $record['last_name'];
+          $_SESSION['email'] = $record['email'];
+          $_SESSION['phone_num'] = $record['phone_num'];
+          $_SESSION['address'] = $record['address'];
+          $_SESSION['address2'] = $record['address2'];
+          $_SESSION['city'] = $record['city'];
+          $_SESSION['postcode'] = $record['postcode'];
+
+          unset($_SESSION['loginError']);//Remove log-in error variable
+          $db_result->free_result();//Free up the space in memory 'locked' by the SQL query result variable
+          header('Location: myaccount.php');//Login is successful, send user to their account page
+        }else{//If no users were found in the db
+          $_SESSION['loginError'] = '<span style="color: red">Incorrect email or password!</span>';//Variable with message in case of an error
+          header('Location: sign_in.php');//Send user back to the Sign In page
+        }
       }else{//If no users were found in the db
         $_SESSION['loginError'] = '<span style="color: red">Incorrect email or password!</span>';//Variable with message in case of an error
         header('Location: sign_in.php');//Send user back to the Sign In page
       }
-    }
 
-    $connection->close();//Close the connection with the db
+      $connection->close();//Close the connection with the db
+    }
+  }
+  catch(Exception $e){
+    echo '<span style="color: red;">Server error! Sorry for the inconvinience, please try again at a different time.</span>';
+    //echo '<br />Developer info: '.$e;
   }
 ?>
